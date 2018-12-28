e96068dcf9c4fa4aec46fa7440f69f2e16e3a1b1
soarkey-PC <soarkey@foxmail.com>
2018-12-28 14:59:07 +0800

    Initial commit

---
+++ src/t1001/Main.java
@@ -0,0 +1,23 @@
+package t1001;
+
+import java.util.Scanner;
+
+/**
+ * 1001 数组中和等于K的数对
+ *
+ * 给出一个整数K和一个无序数组A，
+ * A的元素为N个互不相同的整数，
+ * 找出数组A中所有和等于K的数对。
+ * 例如K = 8，数组A：{-1,6,5,3,4,2,9,0,8}，
+ * 所有和等于8的数对包括(-1,9)，(0,8)，(2,6)，(3,5)。
+ *
+ * @author Soarkey
+ * @date 2018/12/28
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner in =  new Scanner(System.in);
+
+    }
+
+}
